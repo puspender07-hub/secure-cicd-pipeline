95d380a0643a072a49b3dae7788652436b9c1a0b
pushpender07-hub <sachinnda07@gmail.com>
2026-07-13 21:43:33 +0530

    Add Docker and CI/CD pipeline

--- app.py
+++ app.py
@@ -7,4 +7,5 @@ def home():
     return "Welcome to Secure CI/CD Pipeline Project!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+   
---
+++ test_app.py
@@ -0,0 +1,8 @@
+from app import app
+
+def test_home_page():
+    tester = app.test_client()
+    response = tester.get("/")
+
+    assert response.status_code == 200
+    assert b"Welcome to Secure CI/CD Pipeline Project!" in response.data
